0181c58b61bee06f181bcbcd5737546d53d59ad7
hugop <hugopinho07@gmail.com>
2023-07-14 23:22:09 -0300

    added acomodation registry

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const User = require("./models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
 require("dotenv").config();
 const app = express();
 
@@ -13,6 +14,7 @@ const jwtSecret = "agidwhaf56a4wfawfhk56d47awd";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -81,6 +83,17 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const link = req.body.link;
+  console.log(link);
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
 });
 
 app.listen(4000);
